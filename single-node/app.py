905855062bcf88b5003ecfdc447a1f28ee7c38cf
archit.latkar <archit.latkar@cloudera.com>
2022-06-02 11:13:23 +0530

    fixed single-node read and write

--- single-node/app.py
+++ single-node/app.py
@@ -1,9 +1,11 @@
 from crypt import methods
+from unittest import result
 from urllib import response
 from flask import Flask, jsonify,request
 import pymongo
 from pymongo import MongoClient
 import json
+from bson.json_util import loads, dumps
 
 app = Flask(__name__)
 
@@ -11,16 +13,16 @@ client = MongoClient("localhost", 27017)
 
 db = client.my_db
 my_collection = db.people
-query_string = "find()"
-# print(db.command(query_string, "people"))
-# @app.route('/execute', methods=['GET'])
-# def query_executor():
-#     '''
-#     Takes in a query string and executes it as it is
-#     '''
-#     query_string = request.args.get('query')
-#     query_string = "people.find()"
-#     print(db.command(query_string))
+
+@app.route('/execute', methods=['GET'])
+def query_executor():
+    '''
+    collect form data and execute find on it
+    '''
+    data = request.form
+    result = my_collection.find(data)
+    resp = dumps(result)
+    return resp
 
 @app.route('/insert', methods=['POST'])
 def insert():
@@ -28,10 +30,7 @@ def insert():
     Insert document into mongodb
     '''
     data = request.form
-    print(data['name'])
-    data = jsonify(data)
-    print(data)
-    print(type(data))
+    doc = data.to_dict()
     return "got data"
     #my_collection.insert_one()
 
